d3380250518442cb54b0e032a3cf0aeb182faffb
simontv1998 <simontv1998@126.com>
2021-11-18 13:40:08 -0500

    complete mapper.py

--- mapper.py
+++ mapper.py
@@ -3,5 +3,11 @@ import sys
 for line in sys.stdin:
     line = line.strip()
     temperature = int(line[87:92])
-    if ((temperature != 9999)):
-        print('%s\t%d' % (line[15:19], int(line[87:92])))
+    if (temperature == 9999):
+        continue
+    qualityFlag = int(line[92])
+    if (qualityFlag != 0 and qualityFlag != 1 \
+        and qualityFlag != 4 and qualityFlag != 5 \
+        and qualityFlag != 9):
+        continue
+    print('%s\t%d' % (line[15:23], int(line[87:92])))
